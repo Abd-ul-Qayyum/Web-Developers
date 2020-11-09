bc7397e5acb5d70b758f678943232cd13821a412
Abd-ul-Qayyum <a.qayyum1647@gmail.com>
2020-11-09 18:37:21 +0500

    first

---
+++ solution-carriers/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import './assets/style.css';
+import 'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css'
+import 'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD'
+
+function App() {
+    return ( <
+        div className = "box" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+
+        Abdul Qayyum <
+        /header> < /
+        div >
+    );
+}
+
+export default App;
